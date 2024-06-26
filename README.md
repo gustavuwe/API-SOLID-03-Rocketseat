b75aca123491f332123cae97cc9b87c1b6a9d190
Gustav Uwe <gustavuwe.123@gmail.com>
2024-06-26 11:13:39 -0300

    add history of the user check-ins and paginate system

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ GymPass style app.
 - [x] Should be able to authenticate
 - [x] Should be able get the profile of a logged user
 - [ ] Should be able to get the number of check-ins realized by the logged user (like a daystreak)
-- [ ] The user should be able to get his own history of check-ins
+- [x] The user should be able to get his own history of check-ins
 - [ ] The user should be able to search near gym's
 - [ ] The user should be able to search gym's by the name
 - [x] The user should be able to realize a check-in in a gym
@@ -28,5 +28,5 @@ GymPass style app.
 
 - [x] The password of user must be encrypted
 - [x] The application data must be persisted in a database PostgreSQL
-- [ ] All lists of data must be paginated with 20 items per page
+- [x] All lists of data must be paginated with 20 items per page
 - [ ] The user must be identified by a JWT (JSON Web Token)
--- src/repositories/in-memory/in-memory-checkins-repository.ts
+++ src/repositories/in-memory/in-memory-checkins-repository.ts
@@ -1,28 +1,35 @@
 import { Prisma, CheckIn } from "@prisma/client";
 import { CheckInsRepository } from "../check-ins-repository";
 import { randomUUID } from "node:crypto";
-import dayjs from 'dayjs'
+import dayjs from "dayjs";
 
 export class InMemoryCheckInsRepository implements CheckInsRepository {
   public items: CheckIn[] = [];
 
   async findByUserIdOnDate(userId: string, date: Date) {
-    const startOfTheDay = dayjs(date).startOf('date') // 23 00 00
-    const endOfTheDay = dayjs(date).endOf('date') // 23 59 59
+    const startOfTheDay = dayjs(date).startOf("date"); // 23 00 00
+    const endOfTheDay = dayjs(date).endOf("date"); // 23 59 59
 
     const checkInOnSameDate = this.items.find((checkIn) => {
-      const checkInDate = dayjs(checkIn.created_at)
+      const checkInDate = dayjs(checkIn.created_at);
 
-      const isOnSameDate = checkInDate.isAfter(startOfTheDay) && checkInDate.isBefore(endOfTheDay) // check if is in the same day
+      const isOnSameDate =
+        checkInDate.isAfter(startOfTheDay) && checkInDate.isBefore(endOfTheDay); // check if is in the same day
 
-      return checkIn.user_id === userId && isOnSameDate
+      return checkIn.user_id === userId && isOnSameDate;
     });
 
     if (!checkInOnSameDate) {
-        return null
+      return null;
     }
 
-    return checkInOnSameDate
+    return checkInOnSameDate;
+  }
+
+  async findManyByUserId(userId: string, page: number) {
+    return this.items
+      .filter((item) => item.user_id === userId)
+      .slice((page - 1) * 20, page * 20); // slice returns a list between A and B, so 0 and 20 gonna be 0,1,2...19,20  here i implemented a logic that if page 0: 0 * 20 = 0, 1 * 20 = 20, so i can handle the pages with this
   }
 
   async create(data: Prisma.CheckInUncheckedCreateInput) {
